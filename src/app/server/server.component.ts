98b48acfbd13788b5adf92fc1be11331845359e0
euijin-yang <trustnloveu@gmail.com>
2021-05-16 13:00:10 +0900

    Basic of DataBinding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,7 +5,11 @@ import { Component } from "@angular/core";
   templateUrl: './server.component.html'
 })
 
-//* ServerComponent
 export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
